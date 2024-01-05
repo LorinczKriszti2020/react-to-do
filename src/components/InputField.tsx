00414e2ab3cd4b48917c995a76b74ed994fc4b7d
Lorincz Krisztian <lorinczkriszty@gmail.com>
2024-01-05 15:39:38 +0200

    "Finished" base project

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './styles.css'
 
-const InputField = () => {
+interface Props{
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+  handleAdd: (e: React.FormEvent) => void
+}
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null)
+
   return (
-    <form className='input'>
-      <input type="input" placeholder='Enter a task' className='input__box'/>
+    <form className='input' onSubmit={(e) => {
+      handleAdd(e);
+      //blur shifts focus from element. if this is not here the input field
+      //will be selected after submitting with 'enter' and screen will be dark
+      inputRef.current?.blur();
+      }}>
+      <input ref={inputRef}
+             type="input"
+             value={todo}
+             onChange={e => setTodo(e.target.value)}
+             placeholder='Enter a task' 
+             className='input__box'/>
       <button className='input_submit' type='submit'>Add</button>
     </form>
   )
